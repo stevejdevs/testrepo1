21e7705521f2ed45d9086a086d61d4bb7ccebe3a
Damian Bordegaray <damian.bordegaray@gmail.com>
2022-10-29 12:29:39 -0300

    Arreglo Funcion

--- script2.js
+++ script2.js
@@ -1,18 +1,15 @@
 function sumaredades(){
-
-    let inputedad1 = document.getElementById("edad1");
-    let inputedad2 = document.getElementById("edad2");
-    let inputedad3 = document.getElementById("edad3");
+    let inputedad1 = document.getElementById("edad1").value;
+    let inputedad2 = document.getElementById("edad2").value;
+    let inputedad3 = document.getElementById("edad3").value;
     
-    let edad1 = Number(inputedad1.value)
-    let edad2 = Number(inputedad2.value)
-    let edad3 = Number(inputedad3.value)
+    let edad1 = Number(inputedad1);
+    let edad2 = Number(inputedad2);
+    let edad3 = Number(inputedad3);
 
-    let sumaredades= edad1+edad2+edad3
-    
-     alert("La suma de edades es " + sumardades);
+    let total= edad1+edad2+edad3
 
-    
 
+     alert("La suma de edades es " + total );
 
 }
