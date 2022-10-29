4b92de425a44099e9a0158c20839472c06e83ad5
Damian Bordegaray <damian.bordegaray@gmail.com>
2022-10-29 12:23:06 -0300

    Version inicial

---
+++ script2.js
@@ -0,0 +1,18 @@
+function sumaredades(){
+
+    let inputedad1 = document.getElementById("edad1");
+    let inputedad2 = document.getElementById("edad2");
+    let inputedad3 = document.getElementById("edad3");
+    
+    let edad1 = Number(inputedad1.value)
+    let edad2 = Number(inputedad2.value)
+    let edad3 = Number(inputedad3.value)
+
+    let sumaredades= edad1+edad2+edad3
+    
+     alert("La suma de edades es " + sumardades);
+
+    
+
+
+}
